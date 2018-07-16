cd8787e9348ecd4441b70940d9103c3667c04525
jmwofford <jmwofford@gmail.com>
2018-07-16 19:01:55 -0400

    Committed, No Refactor

--- magic.js
+++ magic.js
@@ -3,9 +3,9 @@ console.log("-js connected-")
 
 $('.exampleDiv2').click(function(){
     console.log('Clicked')
-    $('.exDivShow').css({ background: 'blue'})
-    $('.herokuLink').html("GOAT HEROKU")
-    $('.githubLink').html("GitHub Link:https://git.generalassemb.ly/jmwofford/GoatCRUD")
+    $('.exDivShow').css({ backgroundImage:('url(/images/goatt.png)')})
+    $('.herokuLink').html("Heroku Link: https://safe-sea-48470.herokuapp.com/goats")
+    $('.githubLink').html("GitHub Link: https://git.generalassemb.ly/jmwofford/GoatCRUD")
 })
 
 
@@ -20,7 +20,7 @@ $('.exampleDiv7').click(function(){
 
 $('.exampleDiv8').click(function(){
     console.log('Clicked')
-    $('.exDivShow').css({ background: 'green'})
+    $('.exDivShow').css({ backgroundImage:('url(/images/contract.png)')})
     $('.herokuLink').html("Heroku Link (Contract App): ")
     $('.githubLink').html("GitHub Link: https://git.generalassemb.ly/jmwofford/Project3Actual ")
 })
@@ -29,8 +29,29 @@ $('.exampleDiv8').click(function(){
 
 $('.exampleDiv9').click(function(){
     console.log('Clicked')
-    $('.exDivShow').css({ background: 'pink'})
+    $('.exDivShow').css({ background: 'lemonchiffon'})
     $('.herokuLink').html("Others Heroku")
     $('.githubLink').html("Others Git")
 })
 
+// ===========================
+
+
+
+$('.mainCircles').click(function(){
+    console.log('Clicked')
+    $('.subPageMain2').html("As a junior web developer I am looking to develop my skills and abilities to program dynamically rendered web-pages and server-side applications, to increase the efficiency and quality of the applications I build. My sales background allows me to approach this industry with a consumer-driven focus geared towards bridging the cap between highly-functional programs and elegantly-designed UIs . The cornerstone of the successes I intend to reach are fundamentally tied to the ability for me to learn new concepts as a software development professional. I plan to contribute my creative energy and ideas to significantly impacting this industry with exceptional products and applications.")
+   
+})
+
+$('.mainCircles2').click(function(){
+    console.log('Clicked')
+    $('.subPageMain2').html("MissionStatement...loremloremlorem")
+   
+})
+
+$('.mainCircles3').click(function(){
+    console.log('Clicked')
+    $('.subPageMain2').html("Investment Ideas")
+   
+})
